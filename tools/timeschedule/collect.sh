a946e103e0ac90b8b5c19ede5d6c972b66452eb2
Ruben Taelman <rubensworks@gmail.com>
2013-02-14 18:15:31 +0100

    collect.sh info

--- tools/timeschedule/collect.sh
+++ tools/timeschedule/collect.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Dit zal automatisch via een cronjob opgeroepen worden en van iedereen het
+# tijdsschema automatisch samenvoegen en berekenen naar het voorbeeld op twizz
+# TODO...
 file="raw_schedules/ruben.csv"
 
