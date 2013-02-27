5892c271b5b8e520575b82e9d08e592418fd5aa0
Sander Demeester <demeester.sander@gmail.com>
2013-02-27 10:53:04 +0100

    test code to 2.0.4

--- application/project/Build.scala
+++ application/project/Build.scala
@@ -1,21 +1,18 @@
 import sbt._
 import Keys._
-import play.Project._
+import PlayProject._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "application"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "application"
+    val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean
-  )
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      // Add your own project settings here      
+    )
 
 }
