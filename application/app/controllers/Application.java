b32ec4bf48a768b0178fc1be167519c8039b1c53
Sander Demeester <demeester.sander@gmail.com>
2013-03-04 13:49:47 +0100

    Adding new http headers to basic HTTP-response

--- application/app/controllers/Application.java
+++ application/app/controllers/Application.java
@@ -9,8 +9,9 @@ public class Application extends Controller {
   
   public static Result index() {
 	  
-	  
-    return ok(index.render("Your new application is ready."));
+	  response().setHeader("X-Frame-Options", "SAMEORIGIN");
+	  response().setHeader(CACHE_CONTROL, "max-age=3600");
+	  return ok(index.render("Your new application is ready."));
   }
   
 }
