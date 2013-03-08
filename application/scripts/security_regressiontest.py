c5f65095cc18f89d1b7d3aba5da00d83dfac5bbb
Sander Demeester <demeester.sander@gmail.com>
2013-03-08 10:42:48 +0100

    new try

--- application/scripts/security_regressiontest.py
+++ application/scripts/security_regressiontest.py
@@ -13,8 +13,7 @@ from zapv2 import ZAPv2
 
 os.chdir("../")
 null = open("NUL","w")
-
-subprocess_Popen["/opt/play-2.0.4/play", "stage"], stdout=null,stderr=null)
+subprocess.Popen(["/opt/play-2.0.4/play","stage"], stdout=null,stderr=null)
 play_PID = subprocess.Popen(["target/start"], stdout=null,stderr=null)
 
 target = 'http://127.0.0.1:9000'
