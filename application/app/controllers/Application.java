b4f1109913a1122c9656b87b438ebe2f144aba76
Sander Demeester <demeester.sander@gmail.com>
2013-03-11 13:24:54 +0100

    playing with code

--- application/app/controllers/Application.java
+++ application/app/controllers/Application.java
@@ -6,10 +6,21 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends EController {
+	
   
   public static Result index() {
 	  setCommonHeaders();
+	  
 	  return ok(index.render("nanos gigantum humeris insidentes"));
   }
-  
+
+  public static Result test(String id){
+	  
+	  setCommonHeaders();
+	  if(id=="link2")
+		  return ok(test.render("Link2"));
+	  else
+		  return ok(test.render(id));
+		  
+  }
 }
