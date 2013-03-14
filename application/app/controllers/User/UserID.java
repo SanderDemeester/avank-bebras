8cd6d07a92ef46b44f2813de7203eaa5774d8fca
Sander Demeester <demeester.sander@gmail.com>
2013-03-14 14:17:09 +0100

    adding get/set methode in UserID

--- application/app/controllers/User/UserID.java
+++ application/app/controllers/User/UserID.java
@@ -7,5 +7,19 @@ package controllers.User;
 **/
 
 public class UserID {
+	
+	private String userID;
+	
+	public UserID(String UserID){
+		this.userID = userID;
+	}
+	
+	public String geUserID(){
+		return userID;
+	}
+	
+	public void setUserID(String UserID){
+		this.userID = userID;
+	}
 
 }
