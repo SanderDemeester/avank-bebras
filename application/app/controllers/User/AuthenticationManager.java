80a75dc5417d495e2d9d32f33cb45acb4ea2fb2b
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-03-14 14:33:08 +0100

    Add comments and fix whitespace

--- application/app/controllers/User/AuthenticationManager.java
+++ application/app/controllers/User/AuthenticationManager.java
@@ -1,8 +1,10 @@
+
 package controllers.User;
+
 /**
-	Class to handle UserAuthentication.
-	@author Sander Demeester
-**/
+ * Class to handle UserAuthentication.
+ * @author Sander Demeester
+ */
 public class AuthenticationManager {
 
 }
--- application/app/controllers/User/User.java
+++ application/app/controllers/User/User.java
@@ -1,38 +1,64 @@
+
 package controllers.User;
 
 import play.mvc.Result;
 
 /**
- 	The default abstract class for User
-	@author sander
+ * The default abstract class for User.
+ * @author sander
 **/
 
 public abstract class User {
-	
-	private UserID id;
-	private String loginType;
-	
-	public String getUserInfo() {
-		return null;
-	}
-	
-	public void resetPassword(){
-		
-	}
-	
-	public void logout(){
-		
-	}
-	
-	public abstract Result showLandingPage();
-	
-	public Result showStatistics(){
-		//TODO: add proper driver code.
-		return null;
-	}
-	
-	public Result showPersonalInformation(){
-		//TODO: add proper driver code.
-		return null;
-	}
+
+    private UserID id;
+    private String loginType;
+
+    /**
+     * Returns info about this user as a String.
+     * @return Userinfo.
+     */
+    public String getUserInfo() {
+        return null;
+    }
+
+    /**
+     * Reset the password of this user. Is delegated to the
+     * AuthenticationManager.
+     */
+    public void resetPassword(){
+
+    }
+
+    /**
+     * Logs out the user. Is delegated to the AuthenticationManager.
+     */
+    public void logout(){
+
+    }
+
+    /**
+     * The returned page is where the user arrives after login in.
+     * @return The user's landing page.
+     */
+    public abstract Result showLandingPage();
+
+    /**
+     * Redirects the user to the statistics page. This is basically delegated to
+     * the statistics subsystem.
+     * @return The statistics page.
+     */
+    public Result showStatistics(){
+        //TODO: add proper driver code.
+        return null;
+    }
+
+    /**
+     * Redirects the user to the page where he can view and change his personal
+     * information.
+     * @return The personal information page.
+     */
+    public Result showPersonalInformation(){
+        //TODO: add proper driver code.
+        return null;
+    }
 }
