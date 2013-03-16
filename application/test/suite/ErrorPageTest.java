e32f2e5c5f571daa3d895f4f7c6f174b52b32ad7
Sander Demeester <sanderd@zeus.ugent.be>
2013-03-16 14:17:52 +0100

    fix test imports

--- application/test/suite/ErrorPageTest.java
+++ application/test/suite/ErrorPageTest.java
@@ -1,17 +1,9 @@
 package suite;
 
-import static org.junit.Assert.*;
+
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.running;
-
 import org.junit.Test;
-
-import com.sun.jna.platform.win32.W32Service;
-
-import play.mvc.Result;
-import play.test.FakeRequest;
-
-import controllers.routes;
 
 public class ErrorPageTest {
 
