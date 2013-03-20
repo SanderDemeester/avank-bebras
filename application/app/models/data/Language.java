3304db7e07eacfd2b3e678282baddc04d582d7c3
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-03-21 00:22:41 +0100

    Destub languages, but broke tests

--- application/app/models/data/Language.java
+++ application/app/models/data/Language.java
@@ -1,38 +1,132 @@
 package models.data;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+import java.lang.Error;
+
+import play.i18n.Lang;
+import play.i18n.Messages;
+
+import models.data.UnavailableLanguageException;
+import models.data.UnknownLanguageCodeException;
+
 /**
- * Represents a Language that can be used in the application.
- * @author Ruben Taelman
+ * Wrapper class around the Play Lang class. This wrapper adds exceptions to
+ * limit the languages to the supported languages.
+ * @author Felix Van der Jeugt
  */
 public class Language {
 
-    private String code;
-    private String translateCode;
+    private Lang lang;
+
+    // The list of already created languages.
+    private static Set<Language> languages = new TreeSet<Language>();
 
     /**
-     * Creates a new language with a certain ISO Language Code and a key for that language in the messages
-     * @param code    ISO code for the language
-     * @param name    key for the language name in the messages file
+     * Creates a new Language with the given code.
+     * @param code The code Play uses for this language.
      */
-    public Language(String code, String translateCode) {
-        this.code = code;
-        this.translateCode = translateCode;
+    private Language(String code) throws UnknownLanguageCodeException,
+           UnavailableLanguageException {
+        if(code == null || ! code.equals(Lang.forCode(code).code())) {
+            throw new UnknownLanguageCodeException(code);
+        } else if(!Lang.availables().contains(Lang.forCode(code))) {
+            throw new UnavailableLanguageException(code);
+        }
+        this.lang = Lang.forCode(code);
     }
 
     /**
-     * Returns the ISO language code
-     * @return ISO language code
+     * Creates a new Language from the given Lang.
+     * @param lang The Play language.
+     */
+    private Language(Lang lang) throws UnavailableLanguageException {
+        if(!Lang.availables().contains(lang)) {
+            throw new UnavailableLanguageException(lang.code());
+        }
+        this.lang = lang;
+    }
+
+    /**
+     * Creates a new language, or returns an existing language with the given
+     * code.
+     * @param code The code Play uses for this language.
+     */
+    public static Language getLanguage(String code) throws
+            UnavailableLanguageException, UnknownLanguageCodeException {
+        for(Language l : languages) {
+            if(l.getCode().equals(code)) return l;
+        }
+        Language l = new Language(code);
+        languages.add(l);
+        return l;
+    }
+
+    /**
+     * Creates a new language, or returns an existing language as wrapper for
+     * the supplied Lang.
+     * @param code The Play language.
+     */
+    public static Language getLanguage(Lang lang) throws
+            UnavailableLanguageException {
+        for(Language l : languages) {
+            if(l.getLang().equals(lang)) return l;
+        }
+        Language l = new Language(lang);
+        languages.add(l);
+        return l;
+    }
+
+    /**
+     * Returns the readable name of this Language.
+     * @param language The language for the name.
+     * @return The name of this language in the provided language.
+     */
+    public String getName(Language language) {
+        return Messages.get(language.getCode(), "language." + lang.code());
+    }
+
+    /**
+     * Returns the readable name of this Language in the user's preferred
+     * language.
+     * @return The name of this language in the preferred language.
+     */
+    public String getName() {
+        return Messages.get("language." + lang.code());
+    }
+
+    /**
+     * Returns the ISO code for this language.
+     * @return The ISO code for this language.
      */
     public String getCode() {
-        return this.code;
+        return lang.code();
     }
 
     /**
-     * Returns the key for this language from the messages file
-     * @return key for the language name in the messages file
+     * Returns the Play Lang objects this language wraps.
      */
-    public String getTranslateCode() {
-        return this.translateCode;
+    public Lang getLang() {
+        return lang;
+    }
+
+    /**
+     * Lists the available Languages.
+     * @return A List of the Available languages.
+     */
+    public static List<Language> listLanguages() {
+        List<Language> langs = new ArrayList<Language>();
+        try {
+            for(Lang l : Lang.availables()) {
+                langs.add(Language.getLanguage(l));
+            }
+        } catch(UnavailableLanguageException e) {
+            // When we loop over availables...
+            throw new Error("Impossibru!");
+        }
+        return langs;
     }
 
 }
--- application/test/models/question/XmlQuestionTest.java
+++ application/test/models/question/XmlQuestionTest.java
@@ -3,7 +3,7 @@ package models.question;
 import java.util.List;
 
 import models.data.Language;
-import models.data.Languages;
+import models.data.LanguageCreationException;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,22 +34,25 @@ public class XmlQuestionTest {
         }
         Assert.assertNotNull(q);
 
-        // Check the languages
-        Assert.assertTrue(q.getLanguages().contains(Languages.getLanguage("en")));
-        Assert.assertTrue(q.getLanguages().contains(Languages.getLanguage("nl")));
+        try {
+            // Check the languages
+            Assert.assertTrue(q.getLanguages().contains(Language.getLanguage("en")));
+            Assert.assertTrue(q.getLanguages().contains(Language.getLanguage("nl")));
 
-        // Check the titles
-        Assert.assertEquals(q.getTitle(Languages.getLanguage("en")), "A Question");
-        Assert.assertEquals(q.getTitle(Languages.getLanguage("nl")), "Een vraag");
+            // Check the titles
+            Assert.assertEquals(q.getTitle(Language.getLanguage("en")), "A Question");
+            Assert.assertEquals(q.getTitle(Language.getLanguage("nl")), "Een vraag");
 
-        // Check the indexes
-        Assert.assertEquals(q.getIndex(Languages.getLanguage("en")), "index_en.html");
-        Assert.assertEquals(q.getIndex(Languages.getLanguage("nl")), "index_nl.html");
+            // Check the indexes
+            Assert.assertEquals(q.getIndex(Language.getLanguage("en")), "index_en.html");
+            Assert.assertEquals(q.getIndex(Language.getLanguage("nl")), "index_nl.html");
 
-        // Check the feedback
-        Assert.assertEquals(q.getFeedback(Languages.getLanguage("en")), "feedback_en.html");
-        Assert.assertEquals(q.getFeedback(Languages.getLanguage("nl")), "feedback_nl.html");
-
+            // Check the feedback
+            Assert.assertEquals(q.getFeedback(Language.getLanguage("en")), "feedback_en.html");
+            Assert.assertEquals(q.getFeedback(Language.getLanguage("nl")), "feedback_nl.html");
+        } catch(LanguageCreationException e) {
+            Assert.fail(e.getMessage());
+        }
         return q;
     }
 
@@ -61,23 +64,27 @@ public class XmlQuestionTest {
         MultipleChoiceQuestion q = (MultipleChoiceQuestion) testAFile(CORRECT_MC);
         Assert.assertEquals(q.getType(), QuestionType.MULTIPLE_CHOICE);
 
-        // Check the answer contents
-        List<MultipleChoiceElement> elements_en = q.getElements(Languages.getLanguage("en"));
-        Assert.assertEquals(elements_en.get(0).getContent(), "Wrong");
-        Assert.assertEquals(elements_en.get(1).getContent(), "Correct");
-        Assert.assertEquals(elements_en.get(2).getContent(), "Wrong again");
+        try {
+            // Check the answer contents
+            List<MultipleChoiceElement> elements_en = q.getElements(Language.getLanguage("en"));
+            Assert.assertEquals(elements_en.get(0).getContent(), "Wrong");
+            Assert.assertEquals(elements_en.get(1).getContent(), "Correct");
+            Assert.assertEquals(elements_en.get(2).getContent(), "Wrong again");
 
-        // Check the correct answer
-        Assert.assertEquals(elements_en.get(1), q.getCorrectElement(Languages.getLanguage("en")));
+            // Check the correct answer
+            Assert.assertEquals(elements_en.get(1), q.getCorrectElement(Language.getLanguage("en")));
 
-        // Check the answer contents
-        List<MultipleChoiceElement> elements_nl = q.getElements(Languages.getLanguage("nl"));
-        Assert.assertEquals(elements_nl.get(0).getContent(), "Verkeerd");
-        Assert.assertEquals(elements_nl.get(1).getContent(), "Juist");
-        Assert.assertEquals(elements_nl.get(2).getContent(), "Weer verkeerd");
+            // Check the answer contents
+            List<MultipleChoiceElement> elements_nl = q.getElements(Language.getLanguage("nl"));
+            Assert.assertEquals(elements_nl.get(0).getContent(), "Verkeerd");
+            Assert.assertEquals(elements_nl.get(1).getContent(), "Juist");
+            Assert.assertEquals(elements_nl.get(2).getContent(), "Weer verkeerd");
 
-        // Check the correct answer
-        Assert.assertEquals(elements_nl.get(1), q.getCorrectElement(Languages.getLanguage("nl")));
+            // Check the correct answer
+            Assert.assertEquals(elements_nl.get(1), q.getCorrectElement(Language.getLanguage("nl")));
+        } catch(LanguageCreationException e) {
+            Assert.fail(e.getMessage());
+        }
     }
 
     /**
@@ -88,9 +95,13 @@ public class XmlQuestionTest {
         RegexQuestion q = (RegexQuestion) testAFile(CORRECT_REGEX);
         Assert.assertEquals(q.getType(), QuestionType.REGEX);
 
-        // Check regex contents
-        Assert.assertEquals(q.getRegex(Languages.getLanguage("en")), "a wo+rd");
-        Assert.assertEquals(q.getRegex(Languages.getLanguage("nl")), "een wo+rd");
+        try {
+            // Check regex contents
+            Assert.assertEquals(q.getRegex(Language.getLanguage("en")), "a wo+rd");
+            Assert.assertEquals(q.getRegex(Language.getLanguage("nl")), "een wo+rd");
+        } catch(LanguageCreationException e) {
+            Assert.fail(e.getMessage());
+        }
     }
 
     /**
