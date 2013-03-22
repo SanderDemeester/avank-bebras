89572f39062bab550fdd021a8a2926aa2922a53d
Ruben Taelman <rubensworks@gmail.com>
2013-03-22 16:40:50 +0100

    Added some extra features to the editor and some fixes + example of image insertion

--- application/app/controllers/question/editor/QuestionEditorController.java
+++ application/app/controllers/question/editor/QuestionEditorController.java
@@ -3,6 +3,7 @@ package controllers.question.editor;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.EMessages;
 import models.data.Link;
 import models.question.editor.RawQuestion;
 import play.data.Form;
@@ -10,6 +11,7 @@ import play.mvc.Result;
 import views.html.question.editor.create;
 import views.html.question.editor.index;
 import controllers.EController;
+import controllers.question.editor.routes;
 
 /**
  * This is the editor for creating question files, NOT the questions that will be saved in the database
@@ -33,31 +35,39 @@ public class QuestionEditorController extends EController {
         return ok(index.render(breadcrumbs));
     }
     
-    public static Result create(){
+    public static Result create(String type){
         List<Link> breadcrumbs = new ArrayList<Link>();
         breadcrumbs.add(new Link("Home", "/"));
         breadcrumbs.add(new Link("Question Editor", "/questioneditor"));
-        breadcrumbs.add(new Link("Create", "/questioneditor/create"));
+        breadcrumbs.add(new Link("Create "+EMessages.get("question.type."+type), ""));
         
         Form<RawQuestion> questionForm = form(RawQuestion.class);
-        return ok(create.render(breadcrumbs, questionForm));
+        try{
+            questionForm.fill(new RawQuestion(type));
+        } catch(IllegalArgumentException e) {
+            return redirect(routes.QuestionEditorController.index());
+        }
+        return ok(create.render(breadcrumbs, questionForm, type));
     }
     
     public static Result save(){
+        /**
+         * TODO!!!! json save
+         */
         List<Link> breadcrumbs = new ArrayList<Link>();
         breadcrumbs.add(new Link("Home", "/"));
         breadcrumbs.add(new Link("Question Editor", "/questioneditor"));
-        breadcrumbs.add(new Link("Create", "/questioneditor/create"));
+        breadcrumbs.add(new Link("Create", ""));
         
         Form<RawQuestion> questionForm = form(RawQuestion.class).bindFromRequest();
         
         if(questionForm.hasErrors()) {
-            return badRequest(create.render(breadcrumbs, questionForm));
+            return badRequest(create.render(breadcrumbs, questionForm, ""));
         } else {
             breadcrumbs.add(new Link(questionForm.field("addLanguage").value(), "/questioneditor/create"));
         }
         //questionForm.get().save();
-        return ok(create.render(breadcrumbs, questionForm));
+        return ok(create.render(breadcrumbs, questionForm, ""));
         //return redirect(routes.QuestionEditorController.create());
     }
 }
--- application/app/models/question/editor/RawQuestion.java
+++ application/app/models/question/editor/RawQuestion.java
@@ -4,16 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import models.data.Language;
+import models.question.QuestionType;
 import play.db.ebean.Model;
 
 public class RawQuestion extends Model{
     
     public List<String> languages;
+    public List<String> files;
     public String addLanguage;
     public static List<Language> languageList = new ArrayList<Language>();
+    public String type;
     
     static{
         languageList.add(new Language("en", "English"));
         languageList.add(new Language("nl", "Dutch"));
+    }
+    
+    public RawQuestion() {
+        
+    }
+    
+    public RawQuestion(String type) throws IllegalArgumentException {
+        if(QuestionType.valueOf(type)==null)
+            throw new IllegalArgumentException("The provided question type does not exist.");
+        this.type = type;
     }
 }
