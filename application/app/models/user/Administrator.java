40ddead0ebf9895d8567adeda9a9826698d069f9
Jens N. Rammant <jensrammant@gmail.com>
2013-03-22 18:18:20 +0100

    Added stubs for all methods in Admin class

--- application/app/models/user/Administrator.java
+++ application/app/models/user/Administrator.java
@@ -18,5 +18,37 @@ public class Administrator extends Organizer{
     public Administrator(UserID id, Type loginType, String name){
         super(id,loginType,name);
     }
+    
+    /*
+     * returns the management page for the homepage links
+     * @return Homepage Links Management page
+     */
+    public Result manageHomepageLinks(){
+    	return null;
+    }
+    
+    /*
+     * returns the grades page
+     * @return Grades Page
+     */
+    public Result manageGrades(){
+    	return null;
+    }
+    
+    /*
+     * returns difficulties page
+     * @result Difficulties Page
+     */
+    public Result manageDifficulties(){
+    	return null;
+    }
+    
+    /*
+     * return FAQ management page
+     * @return FAQ Management Page
+     */
+    public Result manageFAQ(){
+    	return null;
+    }
 
 }
