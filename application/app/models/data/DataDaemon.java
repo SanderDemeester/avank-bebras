f605817d5c583e898994e36d426cfc0289e5fa1f
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-03-27 23:42:57 +0100

    Complete implementation, see #81
    Warning: This implementation will slowly lag behind. Tasks that are set to run
    on the hour might run about a second after the hour in a year.

--- application/app/models/data/DataDaemon.java
+++ application/app/models/data/DataDaemon.java
@@ -6,7 +6,8 @@ import java.lang.Comparable;
 
 import java.util.PriorityQueue;
 import java.util.Calendar;
-import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * Runs pieces of code at a set time or specified interval.
@@ -22,16 +23,16 @@ public class DataDaemon {
      * @return The DataDaemon
      */
     public static DataDaemon getInstance() {
-        if(instance != null) return instance;
-        return new DataDaemon();
+        if(instance == null) instance = new DataDaemon();
+        return instance;
     }
 
-    private PriorityQueue<Task> queue;
-    private Thread timerThread;
+    private final PriorityQueue<TimedTask> queue;
+    private final Thread timerThread;
 
     // Private constructor.
     private DataDaemon() {
-        queue = new PriorityQueue<Task>();
+        queue = new PriorityQueue<TimedTask>();
         timerThread = new TimerThread();
         timerThread.start();
     }
@@ -42,7 +43,7 @@ public class DataDaemon {
      * @param date When to do it.
      */
     public void runAt(Task task, Calendar date) {
-        // TODO
+        queue.add(new TimedTask(date, 0, task));
     }
 
     /**
@@ -53,7 +54,7 @@ public class DataDaemon {
      * @param date When to start the first run.
      */
     public void repeatedRunAt(Task task, int interval, Calendar date) {
-        // TODO
+        queue.add(new TimedTask(date, interval, task));
     }
 
     /**
@@ -62,18 +63,25 @@ public class DataDaemon {
      * @param interval Interval between two runs.
      */
     public void repeatedRun(Task task, int interval) {
-        // TODO
+        queue.add(new TimedTask(Calendar.getInstance(), interval, task));
     }
 
     /**
      * Runs the provided task at the named occasions, starting at the specified
      * time.
      * @param task What to do.
-     * @param runs When to do it.
+     * @param time When to do it.
      * @param date When to start doing it.
      */
-    public void repeatedRunAt(Task task, Set<Time> runs, Calendar date) {
-        // TODO
+    public void repeatedRunAt(Task task, Time time, Calendar date) {
+        switch(time) {
+            case MIDNIGHT:  date.set(Calendar.SECOND, 0);
+                            date.set(Calendar.MINUTE, 0);
+                            date.set(Calendar.HOUR_OF_DAY, 0);
+                            queue.add(new TimedTask(date, 60 * 60 * 24, task));
+                            break;
+            default:        break;
+        }
     }
 
     /**
@@ -81,7 +89,7 @@ public class DataDaemon {
      * @param task What to do.
      * @param runs When to do it.
      */
-    public void repeatedRun(Task task, Set<Time> runs) {
+    public void repeatedRun(Task task, Time time) {
         // TODO
     }
 
@@ -89,7 +97,7 @@ public class DataDaemon {
         public void run() throws Exception;
     }
 
-    private static class TimedTask implements Comparable<TimedTask> {
+    private static class TimedTask implements Comparable<TimedTask>, Runnable {
 
         /** The time at which this task is set to run. */
         public final Calendar date; // Next run.
@@ -110,29 +118,67 @@ public class DataDaemon {
          */
         public TimedTask next() {
             if(interval <= 0) return null;
-            Calendar newdate = date.clone();
+            Calendar newdate = (Calendar) date.clone();
             newdate.add(Calendar.SECOND, interval);
             return new TimedTask(newdate, interval, task);
+        }
+
+        /**
+         * Returns the time in milliseconds left to the next run.
+         * @return Milliseconds left to date.
+         */
+        public long millisLeft() {
+            return date.getTimeInMillis() -
+                Calendar.getInstance().getTimeInMillis();
         }
 
         @Override public int compareTo(TimedTask that) {
             return this.date.compareTo(that.date);
         }
 
+        @Override public void run() {
+            try { task.run(); }
+            catch(Exception e) { throw new RuntimeException(e); }
+        }
+
     }
 
-    private static class TimerThread extends Thread {
+    private class TimerThread extends Thread {
+        private boolean newTask = false;
+        private Executor executor = Executors.newCachedThreadPool();
+        public void notifyNewTask() {
+            newTask = true;
+            notify();
+        }
         public void run() {
-            TimerTask next = null;
+            TimedTask next = null;
+            long left;
             while(true) {
+                // Check the next task.
                 synchronized(queue) { next = queue.peek(); }
                 try {
+                    // If there's no task ready, just wait indefinitely.
                     if(next == null) wait();
-                    else wait(next.millisLeft()); // TODO implement millisLeft()
+                    else {
+                        // If there's a task ready, wait for it if it isn't
+                        // overdue yet.
+                        left = next.millisLeft();
+                        if(left > 0) wait(left);
+                    }
                 } catch(InterruptedException e) {
+                    // Someone dared interrupt us, just ignore them.
                     continue;
                 }
-                // TODO execute the next task on another thread. (pool?)
+                if(newTask) {
+                    // We were notified of a new task, we loop anew.
+                    newTask = false;
+                    continue;
+                }
+                // Time for the next task: retrieve, execute and repeat it.
+                synchronized(queue) { next = queue.remove(); }
+                executor.execute(next);
+                next = next.next();
+                if(next != null) synchronized(queue) { queue.add(next); }
             }
         }
     }
