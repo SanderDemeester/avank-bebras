1ac05b6450b8909c01d7c67a9461804668606723
Jens N. Rammant <jensrammant@gmail.com>
2013-04-04 15:15:33 +0200

    FAQModel implements Manageable

--- application/app/models/dbentities/FAQModel.java
+++ application/app/models/dbentities/FAQModel.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import models.management.Manageable;
+
 import play.db.ebean.Model;
 
 /**
@@ -15,11 +17,21 @@ import play.db.ebean.Model;
  */
 @Entity
 @Table(name="faq")
-public class FAQModel extends Model {
+public class FAQModel extends Model implements Manageable{
 
 	@Id
 	public int id;
 	public String name;
 	public String language;
 	public String content;
+	
+	@Override
+	public String[] getFieldValues() {
+		String[] res = {language,name};
+		return res;
+	}
+	@Override
+	public String getID() {
+		return Integer.toString(id);
+	}
 }
