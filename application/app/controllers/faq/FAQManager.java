dfdb2fb191fd70a67324fb5ba1c3f63496eff13b
Ruben Taelman <rubensworks@gmail.com>
2013-04-08 16:47:34 +0200

    Updated code that depends on the DMTV.

--- application/app/controllers/faq/FAQManager.java
+++ application/app/controllers/faq/FAQManager.java
@@ -3,12 +3,11 @@
  */
 package controllers.faq;
 
-import controllers.faq.routes;
-import play.db.ebean.Model.Finder;
-import play.mvc.Call;
 import models.EMessages;
 import models.dbentities.FAQModel;
 import models.management.Manager;
+import models.management.ModelState;
+import play.mvc.Call;
 
 /**
  * @author Jens N. Rammant
@@ -18,20 +17,14 @@ public class FAQManager extends Manager<FAQModel> {
 
 	public static final int PAGESIZE = 15;
 	
-	public FAQManager() {
-		super(new Finder<String,FAQModel>(String.class,FAQModel.class), PAGESIZE);
+	public FAQManager(ModelState state) {
+		super(FAQModel.class, state, "id", "id");
 	}
 
 	@Override
 	public String[] getColumnHeaders() {
 		String[] res = {"id",EMessages.get("faq.language"),EMessages.get("faq.name")};
 		return res;
-	}
-
-	@Override
-	public Call getListRoute(int page, String orderBy, String order,
-			String filter) {
-		return routes.FAQController.list(page, orderBy, order, filter);
 	}
 
 	@Override
@@ -48,5 +41,28 @@ public class FAQManager extends Manager<FAQModel> {
 	public Call getRemoveRoute(String id) {
 		return routes.FAQController.remove(id);
 	}
+
+    @Override
+    public Call getListRoute(int page, String filter) {
+        return routes.FAQController.list(page, orderBy, order, filter);
+    }
+
+    @Override
+    public play.api.mvc.Call getSaveRoute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public play.api.mvc.Call getUpdateRoute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getMessagesPrefix() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
