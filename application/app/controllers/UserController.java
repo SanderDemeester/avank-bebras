7f4f58baa9aeaf7e8e242b31608c284b93a9fbf2
Eddy Van Den Heuvel <eddy_vdh@live.be>
2013-04-13 11:15:06 +0200

    import mail

--- application/app/controllers/UserController.java
+++ application/app/controllers/UserController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import controllers.util.Mails;
 import play.api.libs.Crypto;
 import play.data.Form;
 import play.data.format.Formats;
