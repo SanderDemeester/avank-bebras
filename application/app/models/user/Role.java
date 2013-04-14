2cd329f390bcd6608bba2e7d3e8914a9da2b9ca8
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2013-04-14 12:06:59 +0200

    Hide Settings block

--- application/app/models/user/Role.java
+++ application/app/models/user/Role.java
@@ -28,11 +28,12 @@ public class Role {
 
     // Authenticated
     public static Role LANDINGPAGE = new Role();
-    public static Role SETTINGS = new Role(
-        "links.settings.title",
-            new Link(EMessages.get("links.settings.editinfo"), "/editpersonal"),
-            new Link(EMessages.get("links.settings.changepassword"), "/passwedit")
-    );
+    public static Role SETTINGS = new Role();
+    // TODO readd after demo.
+    //    "links.settings.title",
+    //        new Link(EMessages.get("links.settings.editinfo"), "/editpersonal"),
+    //        new Link(EMessages.get("links.settings.changepassword"), "/passwedit")
+    //);
 
     // Organiser
     public static Role MANAGEQUESTIONS = new Role(
