dc7180b14d6d5d1e29adb018256e9f66d2f8edb6
Sander Demeester <demeester.sander@gmail.com>
2013-04-23 23:43:26 +0200

    fix password validation check on client-side, need to test. But will do that tomorrow

--- application/public/javascripts/registration/password_strengthening.js
+++ application/public/javascripts/registration/password_strengthening.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	$("#btn").live("click",function(){
+		if($("#signup").valid()){
 		var password = $("#register_password").val();
 		var salt = CryptoJS.SHA256(password); // eventueel kunnen hier later ook al onze bebras id bepalen en als salt gebruiken voor de hash.
 		var PBKDF2_password = CryptoJS.PBKDF2(password,salt,{ keySize: 4, iterations: 10});
 		$("#register_password").val(PBKDF2_password);
 		$("#controle_passwd").val(PBKDF2_password);
+}else{		
+		return false;
+}
 	});
 });
