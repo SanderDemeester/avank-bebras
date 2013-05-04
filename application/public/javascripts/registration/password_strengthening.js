9169bafd7d4c0f20011168bdeed33e422a8844dd
Sander Demeester <sanderd@zeus.ugent.be>
2013-05-04 13:13:40 +0200

    remove debug output

--- application/public/javascripts/registration/password_strengthening.js
+++ application/public/javascripts/registration/password_strengthening.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
 	
 	$("#btn_reset").live("click",function(){
 		var password = $("#register_password").val();
-		alert(password);
 		var salt = CryptoJS.SHA256(password); // eventueel kunnen hier later ook al onze bebras id bepalen en als salt gebruiken voor de hash.
 		var PBKDF2_password = CryptoJS.PBKDF2(password,salt,{ keySize: 4, iterations: 10});
 		$("#register_password").val(PBKDF2_password);
