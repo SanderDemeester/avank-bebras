ec360aa5c6bad140891ae2484149a04cd5b3aa79
Sander Demeester <sanderd@zeus.ugent.be>
2013-05-05 09:00:32 +0200

    fix validate + remove javascript debug alert message

--- application/public/javascripts/registration/password_strengthening.js
+++ application/public/javascripts/registration/password_strengthening.js
@@ -15,12 +15,15 @@ $(document).ready(function(){
 		
 		
 		alert($("#reset_password").validate().form());
+		if($("#reset_password").validate().form()){
 		var password = $("#r_password").val();
-		alert(password);
 		var salt = CryptoJS.SHA256(password); // eventueel kunnen hier later ook al onze bebras id bepalen en als salt gebruiken voor de hash.
 		var PBKDF2_password = CryptoJS.PBKDF2(password,salt,{ keySize: 4, iterations: 10});
 		$("#r_password").val(PBKDF2_password);
 		$("#controle_passwd").val(PBKDF2_password);
+		}else{
+			return false;
+		}
 		
 	});
 	
