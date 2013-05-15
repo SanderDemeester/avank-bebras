d938fe718dd84e939f818e9d3a19c3cbf637bd7c
Sander Demeester <demeester.sander@gmail.com>
2013-05-15 10:23:51 +0200

    Moving from unmanaged dependencies to managed dependencies

--- application/project/Build.scala
+++ application/project/Build.scala
@@ -9,11 +9,23 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,             
-      "postgresql" % "postgresql" % "9.1-901.jdbc4"
+      "postgresql" % "postgresql" % "9.1-901.jdbc4",
+      "commons-codec" % "commons-codec" % "1.7",
+      "commons-logging" % "commons-logging" % "1.1.1",
+      "org.bouncycastle" % "bcpkix-jdk15on" % "1.48",
+      "org.bouncycastle" % "bcprov-jdk15on" % "1.48",
+      "dom4j" % "dom4j" % "1.6.1",
+      "org.apache.servicemix.bundles" % "org.apache.servicemix.bundles.aspectj" % "1.7.2_1",
+      "stax" % "stax-api" % "1.0.1",
+      "log4j" % "log4j" % "1.2.13",
+      "org.apache.poi" % "poi" % "3.9",
+      "org.apache.xmlbeans" % "xmlbeans" % "2.3.0",
+      "javax.activation" % "activation" % "1.1.1",
+      "javax.mail" % "mail" % "1.4.7"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
-      javacOptions += "-Xlint:all"
+      javacOptions += "-Xlint:all"     
     )
 }
